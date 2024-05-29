ec3d6b308a13b79ca7bec0d0d0d16d83ff480e20
chenchengxin <51522078+zxpoqas123@users.noreply.github.com>
2024-05-29 21:55:25 +0800

    Add files via upload

---
+++ baselines/DAT/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+epochs=60
+lr=1e-3
+loss_type=CE
+optimizer_type=Adam
+batch_size=32
+gpu_id=1
+
+set -ue
+
+# training the network
+seed=2021
+#feature='egemaps'
+feature='FBank'
+#feature='WavLM'
+#feature='mfcc'
+max_len=8
+condition=all
+dataset_type=IEMOCAP_4
+
+python test.py --seed $seed --device-number $gpu_id --feature $feature \
+            --max-len $max_len --lr $lr --batch-size $batch_size \
+            --optimizer-type $optimizer_type --dataset-type $dataset_type --condition $condition 
